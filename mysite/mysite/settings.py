0559a481ab32c07713388389f31f175bcb903585
Magomed Shamaev <ms77grz@gmail.com>
2020-08-04 01:03:42 +0300

    creating forms / handling forms in views / sending emails / rendering forms

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# E-mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('GMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASS')
